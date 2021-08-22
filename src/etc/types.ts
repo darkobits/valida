7b328d09abc3c10a57854a8cb964cf389601f713
Joshua <joshua+github@frontlawn.net>
2021-08-22 01:36:54 -0700

    refactor: Update types.

--- src/etc/types.ts
+++ src/etc/types.ts
@@ -1,47 +1,50 @@
 import type { Options as DeepMergeOptions } from 'deepmerge';
-import type { Ow, BasePredicate, Predicate, NumberPredicate } from 'ow';
+import type { Ow, BasePredicate } from 'ow';
 import type { DeepPartial } from 'ts-essentials';
 
 
 /**
- * Infer the type of a generic predicate.
+ * Deeply wraps a type into predicates.
  */
-export type Infer<T> = T extends BasePredicate<infer P> ? P : T;
-
-export type Deduce<T> = T extends BasePredicate<infer P> ? BasePredicate<P> : T;
-
-
-export type InferDeep<T> = {
-  [K in keyof T]: T[K] extends BasePredicate<infer P>
-    ? P
-    : T[K] extends object
-      ? InferDeep<T[K]>
-      : T[K];
-};
-
-
-export type DeduceDeep<T> = {
-  [K in keyof T]: T[K] extends BasePredicate<infer P>
-    ? BasePredicate<P>
-    : T[K] extends object
-      ? DeduceDeep<T[K]>
-      : T[K];
-};
-
-type UnwrapMe = NumberPredicate & BasePredicate<number | undefined>;
-
-type Foo = Infer<UnwrapMe>;
+export type PredicateFor<T> = T extends BasePredicate ? T : BasePredicate<T>;
 
 
 /**
- * Generic (imperfect) version of the `Shape` type from `ow`.
+ * Deeply wraps a type into predicates. If the type is an object, maps its keys
+ * into predicates while leaving the root intact.
+ *
+ * @example
+ *
+ * type Kitten = {
+ *   cute: boolean;
+ * }
+ *
+ * type KittenPredicate = PredicateObjectFor<Kitten> //=> {
+ *   cute: BasePredicate<boolean>;
+ * }
  */
-export type ShapeFor<T extends Record<string, any>> = {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  [K in keyof T]: T[K] extends object
-    ? (ShapeFor<T[K]> | Predicate<T[K]>)
-    : Predicate<WidenLiterals<T[K]>> | ShapeFor<T[K]>
+export type PredicateObjectFor<T> = T extends BasePredicate ? T : T extends object ? {
+  // For each key in T, if the value is an object, recurse. Otherwise, wrap the
+  // value in BasePredicate.
+  [K in keyof T]: T[K] extends object ? PredicateFor<T[K]> : BasePredicate<T[K]>;
+} : BasePredicate<T>;
+
+
+/**
+ * Deeply unwraps a predicate type or predicate object type into its shape.
+ */
+export type ShapeFor<T> = T extends BasePredicate<infer P> ? P : {
+  // For each key in T, if the value is a BasePredicate, unwrap it. If the value
+  // is an object, recurse. Otherwise, use the type as-is.
+  [K in keyof T]: T[K] extends BasePredicate<infer P> ? P : T[K] extends object ? DefaultsFor<T[K]> : T[K];
 };
+
+
+/**
+ * Deeply unwraps a predicate type into its generic shape, making each property
+ * optional.
+ */
+export type DefaultsFor<T> = DeepPartial<ShapeFor<T>>;
 
 
 /**
@@ -53,6 +56,12 @@ export interface CreateValidatorContext {
 
 
 /**
+ * Optional array merging function. Forwarded to `deepmerge`.
+ */
+export type ArrayMerge = (target: Array<any>, source: Array<any>, options?: DeepMergeOptions) => Array<any>;
+
+
+/**
  * Object returned by a SpecFn.
  */
 export interface CreateValidatorOptions<T extends object = object> {
@@ -61,9 +70,26 @@ export interface CreateValidatorOptions<T extends object = object> {
    * will be used as an `ow` label.
    */
   name?: string;
-  spec: DeduceDeep<T>;
-  defaults?: InferDeep<T>;
-  arrayMerge?: (target: Array<any>, source: Array<any>, options?: DeepMergeOptions) => Array<any>;
+
+  /**
+   * Defines the intended shape for valid objects using `ow` predicates.
+   *
+   * Note: If the root value is a plain object, it will be wrapped in
+   * `partialShape` by default. If your spec requires an exact shape, use
+   * `exactShape` at the root.
+   */
+  spec: PredicateFor<T> | PredicateObjectFor<T>;
+
+  /**
+   * Optional defaults to merge into user-provided values before performing
+   * validation.
+   */
+  defaults?: DefaultsFor<T>;
+
+  /**
+   * Optional array merging function. Forwarded to `deepmerge`.
+   */
+  arrayMerge?: ArrayMerge;
 }
 
 
